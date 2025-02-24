0fe1f1c98860f85276acf7533f522030bff68bed
limasjg <joaoguilhermelima7@gmail.com>
2025-02-24 19:33:41 -0300

    New learnings

---
+++ notes/functions.py
@@ -0,0 +1,26 @@
+#Are block of reusavel codes
+
+# def anime_character(name, power):
+#     print(f"The character os {name} and your power is {power}")
+
+# anime_character("Naruto", "Razengan")
+
+def add(x ,y):
+    z = x + y
+    return z
+
+def sub(x ,y):
+    z = x - y
+    return z
+
+def mult(x ,y):
+    z = x * y
+    return z
+
+def div(x ,y):
+    z = x / y
+    return z
+
+print(sub(5, 9))
+
+# Default arguments can be passed in the parameters when the call is omitted.
